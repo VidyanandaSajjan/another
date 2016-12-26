2f771d9ac4f5645dafedd70c3fadc5942ce9e2f9
VidyanandaSajjan <vidyanandsajjan@gmail.com>
2016-12-26 10:33:29 +0530

    main

---
+++ weather.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by administrator on 7/12/16.
+ */
+
+public class weather {
+    public Coordinates coord=new Coordinates();
+
+    public main main=new main();
+}
